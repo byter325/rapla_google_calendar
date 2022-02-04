126a63c4c5c8d93808cf5723c13ac10feb2c768f
maxomnia <maxreichert@gmx.net>
2022-02-04 12:24:00 +0100

    Reinitalized repo for data security purposes

---
+++ quickstart.py
@@ -0,0 +1,132 @@
+from __future__ import print_function
+import datetime
+from importlib.resources import path
+import pickle
+import os
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import json
+import fs
+
+from rapla_fetch import RaplaFetch
+
+dir = os.path.dirname(os.path.realpath(__file__))
+print(dir)
+settingsReader = open(dir + "/settings.json")
+settings = json.load(settingsReader)
+settingsReader.close()
+print(settings)
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+calendarId = settings['calendar_key']
+
+
+def main():
+    today = datetime.date.today()
+    
+    #Run for the current week
+    run(today)
+
+    #Run for the following week
+    run(today.__add__(datetime.timedelta(days=7)))
+   
+
+def run(date):
+
+    todayAsWeekDay = date.weekday()
+    startOfWeek = date.__sub__(datetime.timedelta(todayAsWeekDay))
+    endOfWeek = date.__add__(datetime.timedelta(days=6 - todayAsWeekDay))
+
+
+    startDateArr = [startOfWeek.day, startOfWeek.month, startOfWeek.year]
+    endDateArr = [endOfWeek.day, endOfWeek.month, endOfWeek.year]
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(dir + '/token.pickle'):
+        with open(dir + '/token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                dir + '/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(dir + '/token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    #calendars = service.calendarList().list().execute()
+    #print(calendars)
+
+    #Fetch from Rapla
+    entries = RaplaFetch().fetch(startDateArr[0], startDateArr[1], startDateArr[2], settings['rapla_url'])
+    googlifiedEntries = []
+
+    #Googlify entries
+    for entry in entries:
+        event = {
+            'summary': entry.title,
+            'location': entry.location,
+            'start' : {
+                'dateTime':convertDateTimeToGoogleFormat(entry.date, entry.startTime),
+                'timeZone':'GMT+01:00'
+            },
+            'end': {
+                'dateTime':convertDateTimeToGoogleFormat(entry.date, entry.endTime),
+                'timeZone':'GMT+01:00'
+            }
+        }
+
+        googlifiedEntries.append(event)
+
+    startDateWithGoogleFormat = convertDateTimeToGoogleQueryFormat(startDateArr, "00:00")
+    endDateWithGoogleFormat = convertDateTimeToGoogleQueryFormat(endDateArr, "00:00")
+
+    print(startDateWithGoogleFormat, endDateWithGoogleFormat)
+
+    #Read entries for week from calendar
+    events_result = service.events().list(calendarId=calendarId, timeMin=startDateWithGoogleFormat, timeMax=endDateWithGoogleFormat,
+                                        timeZone="Europe/Berlin").execute()
+    readEvents = events_result.get('items', [])
+    #clear entries
+    for readEvent in readEvents:
+        print(readEvent)
+        if("(!)" in readEvent['summary']):
+            continue
+        service.events().delete(calendarId=calendarId, eventId = readEvent['id']).execute()
+
+    #insert the new entries
+    for googleEvent in googlifiedEntries:
+        #print(googleEvent)
+        googleEvent = service.events().insert(calendarId=calendarId, body=googleEvent).execute()
+
+def convertDateTimeToGoogleFormat(date, time):
+    dateArr = date.split('.')
+    day = dateArr[0]
+    month = dateArr[1]
+    year = dateArr[2]
+    return year + "-" + month + "-" + day + "T" + time + ":00.000"
+
+def convertDateTimeArrToGoogleFormat(dateArr, time):
+    day = dateArr[0]
+    month = dateArr[1]
+    year = dateArr[2]
+    return str(year) + "-" + str(month) + "-" + str(day) + "T" + str(time) + ":00.000"
+
+def convertDateTimeToGoogleQueryFormat(dateArr, time):
+    day = dateArr[0]
+    month = dateArr[1]
+    year = dateArr[2]
+    return str(year) + "-" + str(month) + "-" + str(day) + "T" + str(time) + ":00+01:00"
+
+if __name__ == '__main__':
+    main()
---
+++ rapla_fetch.py
@@ -0,0 +1,167 @@
+from cgitb import text
+from fs.opener import parse
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+import datetime
+
+class CalendarEntry:
+    title = None
+    date = None
+    startTime = None
+    endTime = None
+    weekDay = None
+    location = "None"
+    def __init__(self) -> None:
+        pass
+    def build(self, title, dateTime, location):
+        self.title = title
+        dateTimeArr = dateTime.split(';')
+        dayAndDate = dateTimeArr[0]
+        time = dateTimeArr[1]
+        self.weekDay = dayAndDate.split(' ')[0]
+        date = dayAndDate.split(' ')[1]
+        times = time.split('-')
+        startTime = times[0]
+        endTime = times[1]
+        self.date = date
+        self.startTime = startTime
+        self.endTime = endTime
+        self.location = location
+        if(location == None):
+            self.location = "None"
+        if(location == "XOnline-Veranstaltung  Virtueller Raum"):
+            self.location = "Online"
+        return self
+
+class IgnoreCourse():
+    def __init__(self, title, weekDay, startTime, endTime) -> None:
+        self.title = title
+        self.weekDay = weekDay
+        self.startTime = startTime
+        self.endTime = endTime
+
+class RaplaFetch():
+    def __init__(self) -> None:
+        self.dayTimePattern = "[A-Z][a-z] [0-2][0-9]:[0-5][0-9]-[0-2][0-9]:[0-5][0-9]"
+        self.dayTimeMatcher = re.compile(self.dayTimePattern)
+        self.dayTimeAndDatePattern = "[A-Z][a-z] [0-3][0-9].[0-1][0-9].[0-2][0-4] [0-2][0-9]:[0-5][0-9]-[0-2][0-9]:[0-5][0-9]"
+        self.courseTitlePattern = "Titel: (\S+\s*)*Sprache"
+        self.courseTitleMatcher = re.compile(self.courseTitlePattern)
+        self.urlYearPattern = "year=\d\d\d\d"
+        self.urlYearMatcher = re.compile(self.urlYearPattern)
+    def fetch(self, day, month, yearParam, urlBase):
+        URL = urlBase + str(day) + "&month="+ str(month) +"&year=" + str(yearParam)
+        print(URL)
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        weekDates = soup.find_all(class_="week_header")
+        table = soup.find(class_="week_table").findChildren()
+        table.pop(0)
+        counter = 0
+        weekDatesStrings = []
+        for date in weekDates:
+            string = date.text
+            weekDatesStrings.append(string)
+        year = str(self.urlYearMatcher.search(URL).group()).replace("year=","")
+        entries = []
+        ignoredCourses = [
+            IgnoreCourse("Medizinisches Grundwissen I (MI)", 'Di', '16:30', '18:00'), 
+            IgnoreCourse("Medizinisches Grundwissen I (MI)", 'Mi', '16:30', '18:00'),
+            IgnoreCourse("Introduction to Networks (CCNA-1, Z)", 'Fr', '13:00', '15:30'),
+            IgnoreCourse("Intercultural Comm Gruppe 2", 'Do', '12:00', '13:30')
+            ]
+
+        totalHours = 0
+        for child in table:
+            block = child.findAll(class_="week_block")
+            if block is not None:
+                for blockChild in block:
+                    aTag = blockChild.find('a')
+                    if aTag is not None:
+                        textArr = aTag.text.replace('\n', " ")
+                        try:
+                            date = self.findDateAsStringFromATag(textArr)[0]
+                            title = self.findCourseTitleFromATag(textArr).strip()
+                            resources = blockChild.findAll(class_="resource")
+                            location = resources[len(resources) - 1].text
+                            if len(date) > 14:
+                                date = self.cleanDate(date)
+                            entry = CalendarEntry().build(title, self.weekDayToDate(date, weekDatesStrings, year), location)
+                            if not self.shouldCourseBeIgnoredByName(entry, ignoredCourses):
+                                totalHours += self.getCourseLength(entry)
+                                print(entry.__dict__)
+                                entries.append(entry)
+                        except:
+                            pass
+                        counter += 1
+
+        print(totalHours/60)
+
+        return entries
+
+    def findDateAsStringFromATag(self, contents):
+        standardPatternResults = re.findall(self.dayTimePattern, contents)
+        if len(standardPatternResults) == 0:
+            standardPatternResults = re.findall(self.dayTimeAndDatePattern, contents)
+        return standardPatternResults
+
+    def findCourseTitleFromATag(self, contents):
+        title = contents.split('Titel:')[1].split("Sprache:")[0]
+        return title
+
+    def cleanDate(self, date):
+        result = re.compile("\d\d.\d\d.\d\d").search(date).group()
+        arr = date.split(result)
+        returnArr = []
+        for item in arr:
+            returnArr.append(item.strip())
+        return " ".join(returnArr)
+
+    def weekDayToDate(self, dayTime, weekDates, year):
+        weekDay = re.compile("[A-Z][a-z]").search(dayTime).group()
+        dayTime = dayTime.replace(weekDay, '').strip()
+        if weekDay == 'Mo':
+            return weekDates[0] + year + ";" + dayTime
+        if weekDay == 'Di':
+            return weekDates[1] + year + ";" + dayTime
+        if weekDay == 'Mi':
+            return weekDates[2] + year + ";" + dayTime
+        if weekDay == 'Do':
+            return weekDates[3] + year + ";" + dayTime
+        if weekDay == 'Fr':
+            return weekDates[4] + year + ";" + dayTime
+        if weekDay == 'Sa':
+            return weekDates[5] + year + ";" + dayTime
+
+        return "Error"
+
+    def shouldCourseBeIgnored(self, courseObj, ignoredCourses):
+        for ignoredCourse in ignoredCourses:
+            if courseObj.title == ignoredCourse.title and courseObj.weekDay == ignoredCourse.weekDay and courseObj.startTime == ignoredCourse.startTime and courseObj.endTime == ignoredCourse.endTime:
+                return True
+        return False
+    def shouldCourseBeIgnoredByName(self, courseObj, ignoredCourses):
+        for ignoredCourse in ignoredCourses:
+            if courseObj.title == ignoredCourse.title:
+                return True
+        return False
+
+    def weekDayDateToFileString(self, weekDayDate, year):
+        return weekDayDate.split(' ')[1].replace('.','_') + year
+
+    def getCourseLength(self, course):
+        startTime = course.startTime.split(':')
+        endTime = course.endTime.split(':')
+
+        startHour = int(startTime[0])
+        startMinutes = int(startTime[1])
+        totalStartTimeInSeconds = startHour * 60 * 60 + startMinutes * 60
+        
+        endHour = int(endTime[0])
+        endMinutes = int(endTime[1])
+        totalEndTimeInSeconds = endHour * 60 * 60 + endMinutes * 60
+
+        diff = totalEndTimeInSeconds - totalStartTimeInSeconds
+        return diff / 60
